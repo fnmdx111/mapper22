85a4079d7651764b94c2041dc7dc0fb869adb3d3
Yi <410517667@qq.com>
2015-10-29 18:21:15 -0400

    Update arrange.m

--- arrange.m
+++ arrange.m
@@ -4,12 +4,8 @@ global BOUNDARY
 global DIAMETER
 global START
 global END
-tolerence=0.06;
-lb=length(BOUNDARY);
-for li=1:lb
-   BOUNDARY(li,1) = BOUNDARY(li,1)*0.9;
-   BOUNDARY(li,2) = BOUNDARY(li,2)*0.9;
-end
+diff=0.15;
+tolerence=0.07;
 SORT=sortrows(BOUNDARY,2); % sort boundary with x
 l=length(SORT); % number of points
 count=floor((SORT(l,2)-SORT(1,2))/DIAMETER); %number of grids
@@ -34,7 +30,7 @@ for i=1:(count+1) % calculate points coordinates
         [~,max_position]=max(temp);
         temp(max_position)=min(temp);
         END(i,2)=START(i,2);
-        END(i,1)=max(temp);
+        END(i,1)=max(temp)-diff;
         if (START(i,2)+DIAMETER)>SORT(l,2)
             break
         end
@@ -48,7 +44,7 @@ for i=1:(count+1) % calculate points coordinates
             end
             [~,max_position]=max(temp);
             temp(max_position)=min(temp);
-            START(i+1,1)=max(temp);
+            START(i+1,1)=max(temp)-diff;
         end
         flag=1;
     else
@@ -61,7 +57,7 @@ for i=1:(count+1) % calculate points coordinates
         [~,min_position]=max(temp);
         temp(min_position)=max(temp);
         END(i,2)=START(i,2);
-        END(i,1)=min(temp);
+        END(i,1)=min(temp)+diff;
         if (START(i,2)+DIAMETER)>SORT(l,2)
             break
         end
@@ -75,9 +71,13 @@ for i=1:(count+1) % calculate points coordinates
             end
             [~,min_position]=min(temp);
             temp(min_position)=max(temp);
-            START(i+1,1)=min(temp);
+            START(i+1,1)=min(temp)+diff;
         end
         flag=0;
     end   
 end
+START(1,2)=START(1,2)+diff;
+END(1,2)=END(1,2)+diff;
+START(count+1,2)=START(count+1,2)-diff;
+END(count+1,2)=END(count+1,2)-diff;
 end
